51f3cd9a07e7cee76d04560d946d35c76ad8787e
Ermek <ermek.abilove@gmail.com>
2024-07-15 22:37:16 +0600

    added initial migration

---
+++ CandidateManager.Infrastructure/Migrations/20240715163338_init.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CandidateManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CallTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LinkedInUrl = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    GitHubUrl = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
---
+++ CandidateManager.WebApi/Controllers/CandidatesController.cs
@@ -0,0 +1,25 @@
+﻿using CandidateManager.Application.DTOs;
+using CandidateManager.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandidateManager.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CandidatesController : ControllerBase
+{
+    private readonly ICandidateService _candidateService;
+
+    public CandidatesController(ICandidateService candidateService)
+    {
+        _candidateService = candidateService;
+    }
+
+    [HttpGet(Name = "GetAllCandidates")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<CandidateDto>>> GetBooks()
+    {
+        var candidates = await _candidateService.GetAllAsync();
+        return Ok(candidates);
+    }
+}
