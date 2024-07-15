2a487998369c85b8c7096c66019263b161b81682
Ermek <ermek.abilove@gmail.com>
2024-07-15 14:56:23 +0600

    Add project files.

---
+++ CandidateManager.Domain/Entities/Candidate.cs
@@ -0,0 +1,16 @@
+﻿using CandidateManager.Domain.Interfaces;
+
+namespace CandidateManager.Domain.Entities;
+
+public class Candidate : IEntityId<int>
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber {  get; set; }
+    public string Email { get; set; }
+    public DateTime? CallTime { get; set; }
+    public string LinkedInUrl { get; set; }
+    public string GitHubUrl { get; set; }
+    public string Comment { get; set; }
+}
